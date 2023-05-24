d2b041abbbf5dd429171e68402d8431fbe56dd18
linasa <lina.b4007@gmail.com>
2023-05-24 16:53:17 +0200

    TESTS : makefile + repertory

---
+++ tests/TestAirWatcher.cpp
@@ -0,0 +1,6 @@
+#include "catch2/catch_amalgamated.cpp"
+
+TEST_CASE( "[Classname] Description du test" ) {
+    REQUIRE( 1 == 1 );
+    REQUIRE( 1 == 0 );
+}
---
+++ tests/TestCaseExample.cpp
@@ -0,0 +1,21 @@
+// 1. g++ -o TestCaseExample TestCaseExample.cpp
+// 2. Run the test file : ./TestCaseExample
+// OPTIONS : -s to see all the successed tests
+
+#include "catch2/catch_amalgamated.cpp"
+
+static int Factorial( int number ) {
+   return number <= 1 ? number : Factorial( number - 1 ) * number;  // fail
+// return number <= 1 ? 1      : Factorial( number - 1 ) * number;  // pass
+}
+
+TEST_CASE( "Factorial of 0 is 1 (fail)", "[single-file]" ) {
+    REQUIRE( Factorial(0) == 0 );
+}
+
+TEST_CASE( "Factorials of 1 and higher are computed (pass)", "[single-file]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
